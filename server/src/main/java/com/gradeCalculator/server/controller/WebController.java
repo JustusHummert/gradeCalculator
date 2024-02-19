d5d760aada09e29839fbd97b7b3f60d096005630
Justus Hummert <justusjhummert@aol.com>
2024-02-19 11:05:38 +0100

    login now redirects user to main and shows sessionId on screen.

--- server/src/main/java/com/gradeCalculator/server/controller/WebController.java
+++ server/src/main/java/com/gradeCalculator/server/controller/WebController.java
@@ -2,6 +2,7 @@ package com.gradeCalculator.server.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -9,5 +10,11 @@ public class WebController {
     @GetMapping("")
     public String login(){
         return "login";
+    }
+
+    @GetMapping("/main")
+    public String mainMenu(@RequestParam String SessionId, Model model){
+        model.addAttribute("SessionId", SessionId);
+        return "main";
     }
 }
